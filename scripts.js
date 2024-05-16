2e6776daf65ea0daee5d698ffa74701d15736849
Murakami52 <qwertyui118123456@mail.ru>
2024-05-16 20:55:19 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,19 +15,4 @@ window.onload = () => {
     setTimeout(() => {
         header.style.opacity = 1;
     }, 500);
-
-    createFallingNuts();
 };
-
-function createFallingNuts() {
-    const nutCount = 20;
-    const fallingNutsContainer = document.querySelector('.falling-nuts');
-
-    for (let i = 0; i < nutCount; i++) {
-        const nut = document.createElement('div');
-        nut.classList.add(i % 2 === 0 ? 'nut' : 'bolt');
-        nut.style.left = `${Math.random() * 100}vw`;
-        nut.style.animationDuration = `${Math.random() * 2 + 3}s`;
-        fallingNutsContainer.appendChild(nut);
-    }
-}
